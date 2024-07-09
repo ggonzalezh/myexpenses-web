3367f13369d7903f339edad3c7e4e8ca12906f43
ggonzalezh <ggonzalezh15@gmail.com>
2024-07-08 20:40:29 -0400

    :sparkles: Added TansStack Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path"
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
+
+const isTest = process.env.NODE_ENV === 'test'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [!isTest && TanStackRouterVite(), react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
