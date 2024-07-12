2ea88808a3f82028b64d6edb45f1dd19ea306c1c
ggonzalezh <ggonzalezh15@gmail.com>
2024-07-12 12:44:03 -0400

    :sparkles: Integrate Post Login Service in Login View

--- src/ui/routes/login.tsx
+++ src/ui/routes/login.tsx
@@ -17,6 +17,18 @@ import {
 import { Input } from '@/ui/components/ui/input.tsx'
 import { Card, CardFooter } from '@/ui/components/ui/card.tsx'
 import { Button } from '@/ui/components/ui/button.tsx'
+import { User } from '@/domain/model/User.ts'
+import { Login } from '@/domain/model/Login.ts'
+import { postLoginUseCase } from '@/common/Configuration.ts'
+import { useMutation } from '@tanstack/react-query'
+
+interface PostLoginUseCase {
+  execute: (login: Login) => Promise<User>
+}
+
+interface LoginProps {
+  postLoginUseCase: PostLoginUseCase
+}
 
 const formSchema = z.object({
   username: z.string().min(4, {
@@ -27,10 +39,15 @@ const formSchema = z.object({
   })
 })
 
-const LoginForm = () => {
-  const context = useRouteContext({ from: '/login' })
-  const { sigIn } = context.authentication
+const LoginForm = ({ postLoginUseCase }: LoginProps) => {
+  const { authentication } = useRouteContext({ from: '/login' })
+  const { sigIn } = authentication
   const navigate = useNavigate()
+
+  const { mutateAsync } = useMutation({
+    mutationFn: (login: Login) => postLoginUseCase.execute(login),
+    onError: () => console.log('ERROR')
+  })
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -43,7 +60,11 @@ const LoginForm = () => {
   const onSubmit = async (
     values: z.infer<typeof formSchema>
   ): Promise<void> => {
-    console.log(values)
+    const user = await mutateAsync({
+      username: values.username,
+      password: values.password
+    })
+    console.log(user)
     sigIn()
     await navigate({ to: '/dashboard' })
   }
@@ -96,5 +117,5 @@ const LoginForm = () => {
 }
 
 export const Route = createFileRoute('/login')({
-  component: LoginForm
+  component: () => <LoginForm postLoginUseCase={postLoginUseCase} />
 })
